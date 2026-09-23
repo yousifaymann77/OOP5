324231373a02c9ce9c3c77cf69c20f31f58a948c
yousif <yshehab693@gmail.com>
2026-09-23 17:18:36 +0300

    Part01-Theoritical  Q1-Object copying

--- oop5Assign/Program.cs
+++ oop5Assign/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main(string[] args)
         {
+            #region Part01 Q1 Object Copying 
+
+            #region a) What happens when you assign one object variable to another object variable?
+            // The variable receives a copy of the refrence to the same object of the first variable  
+            #endregion
+
+            #region b) Does assigning one object to another create a new object? Explain.
+            // No Because The assigning Takes a copy of the refrence of the object so it does not create a new one
+            #endregion
+
+            #region c)What is the difference between copying an object and copying its reference?
+            // Copying a refrence means there is only one object and a two variables with the same refrence to that object 
+            // Copying an object means that there are two different objects with independent memories 
+            #endregion
+
+            #endregion
 
         }
     }
